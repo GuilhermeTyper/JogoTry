009c995c9570c3691bf58bd6c386b9cce8968dd1
guilherme augusto <guilherme augusto>
2017-05-27 02:16:36 -0300

    "comentando o script do jogo"

--- jogo.js
+++ jogo.js
@@ -1,19 +1,22 @@
 var tela = document.querySelector('#tela');
-var c = tela.getContext("2d");
+var c = tela.getContext("2d");//essa parte do código é a definição do nosso pincel
 
-c.fillStyle = "gray";
-c.fillRect(0, 0, 600, 400);
+c.fillStyle = "gray";//definindo a cor do nosso do nosso pincel para desenhar o quadrado 
+c.fillRect(0, 0, 600, 400);//definindo em quadrado
 
 var atira = function(evento) {
-    var x = evento.pageX - tela.offsetLeft;
-    var y = evento.pageY - tela.offsetTop;
+    var x = evento.pageX - tela.offsetLeft; //essa parte do código é uma subtração onde ele pega a posição e diminue
+    var y = evento.pageY - tela.offsetTop; //aqui é a mesma coisa do comentário acima
 
-    c.fillStyle = "blue";
-    c.beginPath();
-    c.arc(x, y, 10, 0,2 * 3.14);
-    c.fill();
+    c.fillStyle = "blue";//definindo a cor da circiferencia
+    c.beginPath();//mandando o nosso pincel iniciar a definição do circulo
+    c.arc(x, y, 10, 0,2 * 3.14);//definindo um circulo
+    c.fill();//mandando o nosso pincel fechar a circurencia
 
     console.log("posição de click: " + x + "," + y);
 }
 
-tela.onclick = atira;
+
+tela.onclick = function(evento) {
+    
+};
